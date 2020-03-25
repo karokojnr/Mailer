ea59ba0c2cd0157768cef5d4b0004b0e8a694ed9
karokojnr <karoko.jnr99@gmail.com>
2020-03-25 12:57:50 +0300

    Initial commit :tada:

---
+++ app.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const exphbs = require("express-handlebars");
+const nodemailer = require("nodemailer");
+const path = require("path");
+const chalk = require("chalk");
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+//MIDDLEWARE
+//View engine setup
+
+app.set("view engine", "handlebars");
+app.engine(
+  "handlebars",
+  exphbs({
+    layoutsDir: __dirname + "/views/",
+    defaultLayout: "contact"
+  })
+);
+
+// Static folder
+app.use("/public", express.static(path.join(__dirname, "public")));
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.render("contact");
+});
+app.post("/send", (req, res) => {
+  const output = `
+    <p>You have a new contact request</p>
+    <h3>Contact Details</h3>
+    <ul>
+    <li>Name: ${req.body.name}</li>
+    <li>Company: ${req.body.company}</li>
+    <li>Email: ${req.body.email}</li>
+    <li>Phone: ${req.body.phone}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${req.body.phone}</p>
+    `;
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "mail.nadab.co.ke",
+    port: 465,
+    secure: true, // true for 465, false for other ports
+    auth: {
+      user: "info@nadab.co.ke", // generated ethereal user
+      pass: "@Nadab19" // generated ethereal password
+    },
+    tls: {
+      rejectUnauthorized: false //running from localhost
+    }
+  });
+
+  // setup email data with unicode symbols
+  let mailOptions = {
+    from: '"Nodemailer Contact" <info@nadab.co.ke>', // sender address
+    to: "kenkarolz99@gmail.com", // list of receivers
+    subject: "Node Contact Request", // Subject line
+    text: "Hello world?", // plain text body
+    html: output // html body
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log("Message sent: %s", info.messageId);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
+    res.render("contact", { msg: "Email has been sent" });
+  });
+});
+
+app.listen(port, () =>
+  console.log("Server running on port:" + chalk.blueBright(`${port}`))
+);
